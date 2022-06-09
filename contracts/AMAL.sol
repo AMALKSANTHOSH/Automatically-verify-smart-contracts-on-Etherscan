8ef8cb62539471ea5e9340b093e7f52c5688ea8a
amal348 <amal.ks@accubits.com>
2022-06-09 18:54:53 +0530

    Revert "FIRST COMMIT"
    This reverts commit 4994d613b0954a9155c4c83adc69d707e3c916ef.

--- contracts/AMAL.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract AMAL is ERC20 {
-    constructor() ERC20("AMAL", "AML") {}
-}
--- migrations/2_initial_migration.js.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function(deployer) {
-  const AMAL = artifacts.require("AMAL");
-
-  await deployer.deploy(AMAL);
-  const contract = await AMAL.deployed();
-
-  console.log("Contract address:", contract.address)
-};
